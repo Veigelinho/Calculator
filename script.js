3655564a5bfad9b34915f3d0c4ca20b9e3c93f56
Marian Veigel <marian.veigel@googlemail.com>
2023-04-23 22:05:21 +0200

    added equal sign to upper dispay part

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const clearButton = document.getElementById('clear')
 const deleteButton = document.getElementById('delete')
 const numberButtons = document.getElementsByClassName('numbers')
 
+var currentOperator = null;
+var lastOperator = null;
+var lastNum = null;
+var currentNum = null;
+
 for (let number of numberButtons) {
     number.onclick = (e) => updateResult(e.target)
 }
 
-var currentOperator = null;
-var secondOperator = true;
 
 for (let operator of operatorButtons) {
     operator.onclick = (e) => {
@@ -27,36 +30,81 @@ for (let operator of operatorButtons) {
 }
 
 function updateResult(input) {
-    console.log(input)
-    if (input.classList == 'operators') {
     
-        if (secondOperator) {
-            displayResult.innerText = `${displayResult.innerText} ${input.innerText} `
-            secondOperator = false;
-        }
-    }
-    else if (input.classList == "numbers") {
+    if (input.classList == "numbers") {
         if (displayResult.innerText == '0')
         displayResult.innerText = input.innerText;
         else {
             displayResult.innerText = `${displayResult.innerText}${input.innerText}`
         }
         secondOperator = true;
+        updateEquasion(input)
     }
+
     
+    else if (input.id == "equalsbutton") {
+        updateEquasion(input)
+    }
+
 }
 
 
 function updateEquasion (input) {
-    calculation.innerText = `${displayResult.innerText} ${input.innerText}`
+    if (input.id == "equalsbutton") {
+        calculation.innerText = displayResult.innerText + " " + input.innerText;
+    }
+    else if (calculation.innerText == "-") {
+        calculation.innerText = input.innerText
+    }
+    else calculation.innerText = displayResult.innerText
 }
 
 
-equalsButton.onclick = (e) => updateEquasion(e.target)
+
+equalsButton.onclick = (e) => updateResult(e.target)
+
 
 
 
 clearButton.onclick = () => {
     displayResult.innerText = 0;
-    calculation.innerText = "";
+    calculation.innerText = "-";
 }
+// if (input.classList == 'operators') {
+        
+//     if (secondOperator) {
+//         lastNum = displayResult.innerText
+//         console.log(lastNum)
+//         displayResult.innerText = `${displayResult.innerText} ${input.innerText} `
+//         secondOperator = false;
+//     }
+// }
+
+// function operate(pressedButton) {
+//     switch (pressedButton.target.innerText) {
+//         case 
+//         case
+//         case
+
+//     }
+// }
+
+
+
+function evaluate(lastNum, currentNum, operator) {
+    switch (operator) {
+        case "+":
+            return lastNum + currentNum;
+        case "-":
+            return lastNum - currentNum;
+        case "×":
+            return lastNum * currentNum;
+        case "÷":
+            if (currentNum == 0) {
+                return "you can't divide by 0"
+            }
+            else return lastNum/currentNum;
+    }
+}
+
+
