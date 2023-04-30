a22dd4773e2fcbac8242e32486ac686bba0a2718
Marian Veigel <marian.veigel@googlemail.com>
2023-04-30 20:07:13 +0200

    eventlisteners now have no callback functions but rather get executed on the spot. Will probably change in future.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const buttons = document.querySelectorAll('button')
 const displayResult = document.getElementById('result')
+
 const operatorButtons = document.getElementsByClassName('operators')
 const equalsButton = document.getElementById('equalsbutton')
 const calculation = document.getElementById('calculation')
+const calculationStartText = "enter your calculation"
 const clearButton = document.getElementById('clear')
 const deleteButton = document.getElementById('delete')
 const numberButtons = document.getElementsByClassName('numbers')
@@ -14,32 +16,84 @@ var lastNum = null;
 var currentNum = null;
 
 for (let number of numberButtons) {
+    lastInputIsOperator = false
     number.onclick = (e) => updateResult(e.target)
 }
 
 
+//  {
+//     operator.onclick = () => {
+        
+        
+//         if (operator.id != "equalsbutton") {
+            
+//             if (!lastInputIsOperator){ 
+                
+//                 if (currentOperator != null) {
+//                     evaluate(lastNum, currentNum, currentOperator)
+//                 }
+                
+//                 else {
+//                     currentNum = displayResult.innerText
+//                     currentOperator = operator.innerText
+//                 }
+//                 // currentNum = displayResult.innerText
+//                 // currentOperator = operator.innerText
+//                 // updateResult(evaluate(lastNum, currentNum, currentOperator))
+//                 // lastInputIsOperator = true
+//             }
+            
+//             if (lastInputIsOperator) {
+//                 currentOperator = operator.innerText
+//                 calculation.innerText = calculation.innerText.slice(0, -1) + operator.innerText
+//             }
+
+
+
+//             else {
+//                 lastNum = displayResult.innerText
+//                 currentOperator = operator.innerText
+//                 updateEquasion(operator)
+//             }
+//             updateResult(operator)
+//         }.innerText {
+    
 for (let operator of operatorButtons) {
-    operator.onclick = (e) => {
+    operator.onclick = () => {
         
-        if (!lastInputIsOperator){ 
-            currentNum = displayResult.innerText
-            currentOperator = operator.innerText
-            updateResult(evaluate(lastNum, currentNum, currentOperator))
-            lastInputIsOperator = true
-        }
         
-        if (lastInputIsOperator) {
 
+        if (displayResult.innerText == 0) {
+            window.alert("Please type in your calculation first.")
         }
 
+        else if (operator.id == "equalsbutton") {
+            
+            if (currentOperator != null) {
+                currentNum = displayResult.innerText
+                evaluate(lastNum, currentNum, currentOperator)
+                currentOperator = null
+            }
+            else if (currentOperator == null) {
+                calculation.innerText = displayResult.innerText + " " + operator.innerText
+                        }
+                    }
 
 
         else {
-            lastNum = displayResult.innerText
-            currentOperator = operator.innerText
-            updateEquasion(operator)
+            
+            if (currentOperator == null) {
+                calculation.innerText = displayResult.innerText + " " + operator.innerText
+                currentOperator = operator.innerText
+                lastNum = displayResult.innerText
+            }
+            else {
+                currentNum = displayResult.innerText
+                evaluate (lastNum, currentNum, operator)
+                calculation.innerText = evaluate (lastNum, currentNum, operator)
+
+            }
         }
-        updateResult(e.target)
     }
 }
 
@@ -60,7 +114,7 @@ function updateResult(input) {
     
     else if (input.id == "equalsbutton") {
         if (lastOperator != null) {
-            currentNum = displayResult.innerText
+            currentNum = displayResult.innerTexts
             updateEquasion(input)
             updateResult(evaluate(currentNum, lastNum, lastOperator))
         }
@@ -89,40 +143,17 @@ function updateEquasion (input) {
 
 
 
-equalsButton.onclick = (e) => {
-    evaluate()
-    updateResult(e.target)
-}
-
-
 
 clearButton.onclick = () => {
     displayResult.innerText = 0;
     calculation.innerText = "enter your calculation";
 }
-// if (input.classList == 'operators') {
-        
-//     if (secondOperator) {
-//         lastNum = displayResult.innerText
-//         console.log(lastNum)
-//         displayResult.innerText = `${displayResult.innerText} ${input.innerText} `
-//         secondOperator = false;
-//     }
-// }
 
-// function operate(pressedButton) {
-//     switch (pressedButton.target.innerText) {
-//         case 
-//         case
-//         case
-
-//     }
-// }
 
 
 
 function evaluate(num1, num2, operator) {
-    switch (operator) {
+    switch (operator.innerText) {
         case "+":
             return num1 + num2;
         case "-":
